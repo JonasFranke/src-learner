d23c2951dbca4a0d5eab84852a69a021883850f1
Jonas Franke <info@jonasfranke.xyz>
2025-10-01 13:07:55 +0200

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Bun dev env";
+
+  inputs = { nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable"; };
+
+  outputs = { self, nixpkgs, ... }@inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        nativeBuildInputs = [ pkgs.bun pkgs.biome ];
+
+        shellHook = ''
+          echo "Bun $(bun --version) and Biome $(biome --version) are ready!" &&
+          export BIOME_BINARY=$(which biome)
+        '';
+      };
+
+    };
+}
